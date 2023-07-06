43b426fe089d72d9383939a7b49009de2a66c668
Jack Rive <jack.rive2@gmail.com>
2023-07-06 10:14:07 +0100

    fixed bug where processes opened new tk windows

--- resizing_tool.py
+++ resizing_tool.py
@@ -7,50 +7,51 @@ import multiprocessing
 
 import img_resizer
 
+if __name__ == '__main__':
 
-source_dir = ""
-target_dir = ""
-target_size = 250
-
-
-window = tk.Tk()
-window.geometry("500x250")
+    source_dir = ""
+    target_dir = ""
+    target_size = 250
 
 
-def UploadAction(s_t):
-    directory = filedialog.askdirectory()
-    if s_t == 0:
-        global source_dir
-        source_dir = directory
-        button_source_dir.configure(text=source_dir)
-    else:
-        global target_dir
-        target_dir = directory
-        button_target_dir.configure(text=target_dir)
+    window = tk.Tk()
+    window.geometry("500x250")
 
 
-def Run(size):
+    def UploadAction(s_t):
+        directory = filedialog.askdirectory()
+        if s_t == 0:
+            global source_dir
+            source_dir = directory
+            button_source_dir.configure(text=source_dir)
+        else:
+            global target_dir
+            target_dir = directory
+            button_target_dir.configure(text=target_dir)
 
-    if source_dir == "" or target_dir == "":
-        messagebox.showerror('Python Error', 'Error: Invalid source or target directory!')
-    else:
-        global target_size
-        target_size = size
 
-        print("Source dir = " + source_dir)
-        print("Target dir = " + target_dir)
-        print("Target size = " + str(target_size))
-        
-        button_go.place(relx=0.45, rely=1.2)
-        pb.place(relx=0.22, rely=0.8)
-        
-        pb.start()
+    def Run(size):
 
-        sub_arr1, sub_arr2, sub_arr3, sub_arr4 = img_resizer.split(source_dir)
-        
+        if source_dir == "" or target_dir == "":
+            messagebox.showerror('Python Error', 'Error: Invalid source or target directory!')
+        else:
+            global target_size
+            target_size = size
 
-        # start processes:
-        if __name__ == '__main__':
+            print("Source dir = " + source_dir)
+            print("Target dir = " + target_dir)
+            print("Target size = " + str(target_size))
+            
+            button_go.place(relx=0.45, rely=1.2)
+            pb.place(relx=0.22, rely=0.8)
+            
+            pb.start()
+
+            sub_arr1, sub_arr2, sub_arr3, sub_arr4 = img_resizer.split(source_dir)
+            
+
+            # start processes:
+            
 
             pipe1_1, pipe1_2 = multiprocessing.Pipe()
             pipe2_1, pipe2_2 = multiprocessing.Pipe()
@@ -73,45 +74,45 @@ def Run(size):
             pipe3_1.recv()
             pipe4_1.recv()
 
-        pb.stop()
+            pb.stop()
 
-        window.destroy()
+            window.destroy()
 
-        messagebox.showinfo('Done', 'All files in target directory processed!')
-
-
-l1 = tk.Label(window, text="Source directory: ")
-button_source_dir = tk.Button(window, text='Open', width=55, command=lambda: UploadAction(0))
-
-l2 = tk.Label(window, text="Target directory: ")
-
-button_target_dir = tk.Button(window, text='Open', width=55, command=lambda: UploadAction(1))
-
-l3 = tk.Label(window, text="File size (kb): ")
-
-size = tk.Scale(window, from_=1, to=1000, length=390, orient='horizontal')
-size.set(250)
-
-global button_go
-button_go = tk.Button(window, text='compress', background='greenyellow', command=lambda: Run(size.get()))
-
-global pb
-pb = ttk.Progressbar(window, orient='horizontal', mode='indeterminate', length=280)
+            messagebox.showinfo('Done', 'All files in target directory processed!')
 
 
+    l1 = tk.Label(window, text="Source directory: ")
+    button_source_dir = tk.Button(window, text='Open', width=55, command=lambda: UploadAction(0))
 
-l1.place(relx=0, rely=0)
-#l1.pack()
-button_source_dir.place(relx=0.2, rely=0)
+    l2 = tk.Label(window, text="Target directory: ")
 
-l2.place(relx=0, rely=0.2)
-button_target_dir.place(relx=0.2, rely=0.2)
+    button_target_dir = tk.Button(window, text='Open', width=55, command=lambda: UploadAction(1))
 
-l3.place(relx=0, rely=0.4)
-size.place(relx=0.2, rely=0.33)
+    l3 = tk.Label(window, text="File size (kb): ")
 
-button_go.place(relx=0.45, rely=0.8)
+    size = tk.Scale(window, from_=1, to=1000, length=390, orient='horizontal')
+    size.set(250)
 
-pb.place(relx=0.22, rely=1.2)
+    global button_go
+    button_go = tk.Button(window, text='compress', background='greenyellow', command=lambda: Run(size.get()))
 
-window.mainloop()
+    global pb
+    pb = ttk.Progressbar(window, orient='horizontal', mode='indeterminate', length=280)
+
+
+
+    l1.place(relx=0, rely=0)
+    #l1.pack()
+    button_source_dir.place(relx=0.2, rely=0)
+
+    l2.place(relx=0, rely=0.2)
+    button_target_dir.place(relx=0.2, rely=0.2)
+
+    l3.place(relx=0, rely=0.4)
+    size.place(relx=0.2, rely=0.33)
+
+    button_go.place(relx=0.45, rely=0.8)
+
+    pb.place(relx=0.22, rely=1.2)
+
+    window.mainloop()
